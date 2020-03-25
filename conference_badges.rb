ff7bc06899e95394702c0f3f3c4c70d826b9e66d
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-25 18:49:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,10 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(name)
+  batch = []
+  batch_badge_creator.each do |speaker|
+    batch << "Hello, my name is #{speaker}."
+  end
+end
